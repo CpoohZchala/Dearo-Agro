07a393dc1255973240a736401dd84da795c61be0
Cpooh <cpooh2001@gmail.com>
2025-03-19 11:30:09 +0530

    fixed code

--- lib/views/cultivationalDetailsAdd.dart
+++ lib/views/cultivationalDetailsAdd.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CultivationalAddScreen extends StatefulWidget {
   const CultivationalAddScreen({super.key});
@@ -19,25 +20,63 @@ class _CultivationalAddScreenState extends State<CultivationalAddScreen> {
             ClipPath(
               clipper: ArcClipper(),
               child: Container(
-                  height: 190,
-                  color: const Color.fromRGBO(87, 164, 91, 0.8),
-                  child: Center(
-                      child: Row(children: [
-                    IconButton(
+                height: 190,
+                color: const Color.fromRGBO(87, 164, 91, 0.8),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
                         onPressed: () {
                           Navigator.pop(context);
                         },
-                        icon: const Icon(Icons.arrow_back_ios),),
-                       const SizedBox(width:270),
-                       Image.asset("assets/icons/man.png",
-                       width: 35,height: 35,),
-                       ],
-                  
+                        icon: const Icon(Icons.arrow_back_ios),
+                      ),
+                      Image.asset(
+                        "assets/icons/man.png",
+                        width: 35,
+                        height: 35,
+                      ),
+                    ],
                   ),
-                  ),
-                  ),
+                ),
+              ),
             ),
+            const SizedBox(height: 5),
+
+            _buildTextField("Type Your Member ID "),
+            _buildTextField("Select Your Crop Category"),
+            _buildTextField("Select Your Crop"),
+            _buildTextField("Location Address Type here"),
+            _buildTextField("Start Date "),
+            _buildTextField("Select Your Destrict"),
+            _buildTextField("Select Your City")
           ],
+        ),
+      ),
+    );
+  }
+
+  // **Reusable Text Field**
+  Widget _buildTextField(String label) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: label,
+          labelStyle: GoogleFonts.poppins(fontSize: 15),
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              
+                color: Color.fromRGBO(19, 146, 25, 0.8), width: 3),
+          ),
         ),
       ),
     );
