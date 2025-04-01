a9d47870559f48ece4c2b97920ccc5743f5ec9e7
Cpooh <cpooh2001@gmail.com>
2025-04-01 09:51:40 +0530

    updated user profile

--- lib/screens/dialogBox/deleteProfile_dialog.dart
+++ lib/screens/dialogBox/deleteProfile_dialog.dart
@@ -1,59 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void showDeleteProfileDialog(BuildContext context) {
-  showDialog(
+Future<void> showDeleteProfileDialog(BuildContext context) {
+  return showDialog<void>(
     context: context,
+    barrierDismissible: false,
     builder: (BuildContext context) {
       return AlertDialog(
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [    
-            Text(
-              "Are you sure you want to delete your account? ",
-              textAlign: TextAlign.center,
-              style: GoogleFonts.poppins(
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-             Text(
-              "This action cannot be undone.",
-              textAlign: TextAlign.center,
-              style: GoogleFonts.poppins(
-                fontSize: 12,
-                fontWeight: FontWeight.normal,
-              ),
-            ),
-            const SizedBox(height: 50),
-            Row(
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromRGBO(87, 164, 91, 0.8),
-                      foregroundColor: Colors.white),
-                  onPressed: () {},
-                  child: Text(
-                    "No",
-                    style: GoogleFonts.poppins(),
-                  ),
-                ),
-                const Spacer(),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      foregroundColor: Colors.white),
-                  onPressed: () {},
-                  child: Text(
-                    "Yes",
-                    style: GoogleFonts.poppins(),
-                  ),
-                )
-              ],
-            ),
-          ],
-        ),
+        title: Text('Confirm Delete Account',
+        style: GoogleFonts.poppins(fontWeight: FontWeight.bold),),
+        content:  Text('Are you sure you want to delete your account?',
+         style: GoogleFonts.poppins()),
+        actions: <Widget>[
+          TextButton(
+            child:  Text('No',
+             style: GoogleFonts.poppins(color: Colors.green)),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          TextButton(
+            child: Text('Yes', style: GoogleFonts.poppins(color: Colors.red)),
+            onPressed: ()  {
+            
+            },
+          ),
+        ],
       );
     },
   );
